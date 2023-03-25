f232076c0657d95115ba4ede57eb16366e7b3310
MarwanOladele <oladelemarwan@gmail.com>
2023-03-25 16:25:46 +0100

    new commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,6 +22,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer ">
             Marwan
+            
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10 ">
