dcc1af3204daa2f67497e511ae1b58524b69f295
MarwanOladele <oladelemarwan@gmail.com>
2023-05-07 09:38:03 +0100

    Tech Stack Animation Fastened

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -9,7 +9,7 @@ const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology, index) => (
-        <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)} className="w-28 h-28 rounded-full bg-secondary flex justify-center items-center" key={technology.name}>
+        <motion.div variants={fadeIn('up', 'spring', index * 0.25, 0.75)} className="w-28 h-28 rounded-full bg-secondary flex justify-center items-center" key={technology.name}>
           <img src={technology.icon} alt={technology.name} className='w-[70px] h-[70px] contain'/>
         </motion.div>
       ))}
